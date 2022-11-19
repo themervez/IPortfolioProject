fc1bc8521250176e719f2819471a856b021efa23
Merve ÖZ <104096244+themervez@users.noreply.github.com>
2022-11-19 20:13:42 +0300

    Codes by training

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoUpSchoolProject.Models.Entities;
+
+namespace DemoUpSchoolProject.Controllers
+{
+    public class MemberController : Controller
+    {
+        UpSchoolDbPortfolioEntities db = new UpSchoolDbPortfolioEntities();
+        public ActionResult Index()
+        {
+            var mail = Session["MemberMail"].ToString();
+            var values = db.TblMember.Where(x => x.MemberMail == mail).FirstOrDefault();//Sisteme giriş yapan kullanıcının bilgilerini getirdi values değişkeni ile birlikte
+            ViewBag.name = values.MemberName;
+            ViewBag.surname = values.MemberSurname;
+            ViewBag.id = values.MemberID;
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/StatisticController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoUpSchoolProject.Models.Entities;
+namespace DemoUpSchoolProject.Controllers
+{
+    public class StatisticController : Controller
+    {
+        UpSchoolDbPortfolioEntities db = new UpSchoolDbPortfolioEntities();
+        public ActionResult Index()
+        {   //Toplam referans veri sayısı
+            ViewBag.v1 = db.TblTestimonial.Count();
+            //Şehir bilgisi İstanbul olan referans sayısı
+            ViewBag.v2 = db.TblTestimonial.Where(x => x.City == "İstanbul").Count();
+            //Android Developer haricindeki kişi sayısı
+            ViewBag.v3 = db.TblTestimonial.Where(x => x.Profession != "Android Developer").Count();
+            //Şehri Mersin olan kişinin ismi
+            ViewBag.v4 = db.TblTestimonial.Where(x => x.City == "Mersin").Select(y => y.NameSurname).FirstOrDefault();
+            //Referansların ortalama maaşı
+            ViewBag.v5 = db.TblTestimonial.Average(x => x.Salary);
+            return View();
+        }
+    }
+}
